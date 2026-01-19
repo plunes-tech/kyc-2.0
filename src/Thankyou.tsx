013afa44fbc67441cd3ab84c3de1717bd27501e9
Rathore-Jai <jai.rathore@plunes.com>
2026-01-19 19:59:09 +0530

    added: thankyou and landing page

--- src/Thankyou.tsx
+++ src/Thankyou.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { scrollToTop } from "./utils/utilits";
 
-const Thankyou:React.FC = () => {
+const Thankyou: React.FC = () => {
 
     useEffect(() => {
         scrollToTop()
@@ -9,25 +9,25 @@ const Thankyou:React.FC = () => {
 
     return (
         <>
-            <div className={`bg-[url("https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-background.webp")]
-                w-[100vw] h-[100vh] bg-no-repeat flex flex-col items-center justify-center absolute top-0 left-0 z-1000`}
+            <main className={`bg-[url("https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-background.webp")]
+                w-[100vw] h-[100vh] bg-[length:100%_100%] bg-no-repeat flex flex-col items-center justify-center absolute top-0 left-0 z-1000`}
             >
                 <div className="container">
-                    <img className="block m-auto h-[2rem] sm:h-[3.5rem] md:h-[4.5rem]" 
+                    <img className="block m-auto h-[2rem] sm:h-[3.5rem] md:h-[4.5rem]"
                         src={
-                            window.innerWidth > 567 ? "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-web.png" 
-                            : "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-mob.png"
-                        } 
+                            window.innerWidth > 567 ? "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-web.png"
+                                : "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-mob.png"
+                        }
                     />
                     <h1 className="text-[1.75rem] text-[#022E85] text-center font-bold mt-8">Thank you for completing your KYC!</h1>
                     <h2 className="text-[1rem] text-[#222222] text-center">Your KYC verification is now complete!</h2>
                     <p className="text-[0.8rem] text-[#7C8087] text-center w-[90%] md:w-[70%] mx-auto mt-4">
-                        We appreciate you verifying your identity. Your KYC process has been successfully completed, 
-                        and no further action is required at this time. If we need any additional information, 
+                        We appreciate you verifying your identity. Your KYC process has been successfully completed,
+                        and no further action is required at this time. If we need any additional information,
                         we will reach out to you.
                     </p>
                 </div>
-            </div>
+            </main>
         </>
     )
 }
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,64 +1,79 @@
 import React, { useEffect, useState } from "react";
 import { getMobileOperatingSystem } from "../utils/utilits";
+import { Grid } from "@mantine/core";
+import { AndroidIcon, AppleIcon } from "../assets/icons";
 
 const Landing: React.FC = () => {
 
     const [appLink, setAppLink] = useState('https://play.google.com/store/apps/details?id=com.plunes&amp;hl=en_IN/')
 
     useEffect(() => {
-        if(getMobileOperatingSystem() === "iOS") {
+        if (getMobileOperatingSystem() === "iOS") {
             setAppLink('https://apps.apple.com/us/app/plunes/id1463747553/')
         }
     }, [])
 
     return (
         <main className={`bg-[url("https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-background.webp")]
-            bg-no-repeat w-[100vw] h-[100vh] flex items-center justify-center flex-col absolute top-0 left-0 z-1000`}
+            w-[100vw] min-h-[100vh] bg-[length:100%_100%] bg-no-repeat flex flex-col items-center justify-center absolute top-0 left-0 z-1000`}
         >
             <div className="container">
-                <img className="w-full m-auto" 
-                    src={window.innerWidth > 567 ? 
-                    "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-web.png" : 
-                    "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-mob.png"} 
+                <img className="block m-auto h-[2rem] sm:h-[3.5rem] md:h-[4.5rem]"
+                    src={
+                        window.innerWidth > 567 ? "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-web.png"
+                            : "https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-mob.png"
+                    }
                 />
-                <h1 className="mt-8 text-center text-[#022E85] font-extrabold text-[3.5rem]">Thank You For Giving Your Details.</h1>
-                <h2 className="text-[#111111] text-center mt-4 text-[2.25rem]">Our Team of Experts will Call You Back Soon!</h2>
-                <div className="m-auto mt-8 items-center justify-center w-[60%] row">
-                    <div className="col-lg-auto">
-                        <img className="w-full" src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-scan-code.webp"/>
-                    </div>
-                    <div className="col-lg-auto download-btn">
-                        <a href={appLink} target="_blank" title="app store">Download The App <i className="fab fa-apple" />|<i className="fab fa-android"/></a>
+                <h1 className="mt-8 text-center text-[#022E85] font-extrabold text-[1.75rem]">Thank You For Choosing Plunes.</h1>
+                <div className="flex flex-wrap items-center justify-center gap-[2rem] m-auto mt-8">
+                    <img className="h-[8rem] w-auto" src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-scan-code.webp" />
+                    <div className="flex items-center gap-1.5 text-[#FFFFFF] text-[1rem] bg-[#14B961] hover:bg-[#129F53] px-6 py-1.25 rounded-[5rem] cursor-pointer"
+                        onClick={() => {
+                            window.open(appLink, "_blank")
+                        }}
+                    >
+                        Download The App 
+                        <div className="flex items-center gap-1">
+                            <AppleIcon width={20} height={20}/>
+                            <div className="h-4 w-0.25 bg-[#FFFFFF] rounded-[10rem]"/>
+                            <AndroidIcon width={20} height={20}/>
+                        </div>
                     </div>
                 </div>
-                <div className="download-reasons">
-                    <h3>Download Plunes App to:</h3>
-                    <div className="row">
-                        <div className="col-lg-3 col-12 row">
-                            <div className="col-lg-4 col-auto">
-                                <img src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-1.png" className="w-full" />
+                <div className="mt-8 mx-auto md:w-[700px]">
+                    <h3 className="text-center text-[#232323] font-semibold text-[1.25rem]">Download Plunes App to:</h3>
+                    <Grid grow align="start" mt={32} p={16}>
+                        <Grid.Col span={{base: 12, sm: 6, md: 4, lg: 4}}>
+                            <div className="flex items-center gap-1.5">
+                                <img src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-1.png" 
+                                    className="h-[2.5rem]"
+                                />
+                                <p className="text-[0.8rem] text-[#7C8087]">
+                                    Plan Your Surgery
+                                </p>
                             </div>
-                            <div className="col-lg-8 col-6">
-                                <span>Plan your Surgery</span>
+                        </Grid.Col>
+                        <Grid.Col span={{base: 12, sm: 6, md: 4, lg: 4}}>
+                            <div className="flex items-center gap-1.5">
+                                <img src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-2.png" 
+                                    className="h-[2.5rem]"
+                                />
+                                <p className="text-[0.8rem] text-[#7C8087]">
+                                    Stay Updated with Your Treatment Journey
+                                </p>
                             </div>
-                        </div>
-                        <div className="col-lg-3 col-12 row">
-                            <div className="col-lg-4 col-auto">
-                                <img src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-2.png" className="w-full" />
+                        </Grid.Col>
+                        <Grid.Col span={{base: 12, sm: 6, md: 4, lg: 4}}>
+                            <div className="flex items-center gap-1.5">
+                                <img src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-3.png" 
+                                    className="h-[2.5rem]"
+                                />
+                                <p className="text-[0.8rem] text-[#7C8087]">
+                                    Connect to Expert Doctors Personally
+                                </p>
                             </div>
-                            <div className="col-lg-8 col-6">
-                                <span>Stay Updated with Your Treatment Journey</span>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-12 row">
-                            <div className="col-lg-4 col-auto">
-                                <img src="https://service-family-images.s3.ap-south-1.amazonaws.com/website_images_new/thank-you-logo-3.png" className="w-full" />
-                            </div>
-                            <div className="col-lg-8 col-6">
-                                <span>Connect to Expert Doctors Personally</span>
-                            </div>
-                        </div>
-                    </div>
+                        </Grid.Col>
+                    </Grid>
                 </div>
             </div>
         </main>
